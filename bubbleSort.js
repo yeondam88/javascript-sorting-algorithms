da00198db694f564b754e5efb037241642c89697
Yeondam Park <giyen9088@gmail.com>
2018-12-06 21:35:50 -0800

    adding sorting algorithms

---
+++ bubbleSort.js
@@ -0,0 +1,25 @@
+/**
+  Bubble Sort
+  Time complexicy - O(n2)
+ */
+
+function bubbleSort(arr) {
+  // [2,5,1,39,23] -> compare the element start from left to right and
+  // if the left element is bigger than next element then swap the value
+
+  let change; // This varaible used when the last elements are already sorted, then it uses to break the loop
+  for (let i = 0; i < arr.length; i++) {
+    change = false;
+    for (let j = 0; j < arr.length - (i + 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        change = true;
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+    if (!change) break;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([2, 5, 1, 39, 23])); // [1,2,5,23,39]
---
+++ mergeSort.js
@@ -0,0 +1,49 @@
+/**
+  merge Sort
+  Time complexicy - O(n log n)
+ */
+
+// helper function - arrays are need to be sorted before run this function
+// [1,5,7], [4,7,12]
+
+function merge(arr1, arr2) {
+  let indexA = 0;
+  let indexB = 0;
+  let results = [];
+
+  while (indexA < arr1.length && indexB < arr2.length) {
+    if (arr1[indexA] < arr2[indexB]) {
+      results.push(arr1[indexA]);
+      indexA++;
+    } else {
+      results.push(arr2[indexB]);
+      indexB++;
+    }
+  }
+
+  while (indexA < arr1.length) {
+    // when the array is exhausted, push all leftover elements
+    results.push(arr1[indexA]);
+    indexA++;
+  }
+
+  while (indexB < arr2.length) {
+    // when the array is exhausted, push all leftover elements
+    results.push(arr2[indexB]);
+    indexB++;
+  }
+
+  return results;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid)); // slice out from index 0 to before mid [1,3,5,2,6] -> [1,3]
+  let right = mergeSort(arr.slice(mid)); // slice out from index mid to end [1,3,5,2,6] -> [5,2,6]
+
+  return merge(left, right);
+}
+
+console.log(mergeSort([2, 5, 1, 39, 23]));
