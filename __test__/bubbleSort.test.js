220f41ab04d1cd158eeb7bc672a762b7ee05936f
Yeondam Park <giyen9088@gmail.com>
2018-12-07 19:35:41 -0800

    re-structure the file

--- __test__/bubbleSort.test.js
+++ __test__/bubbleSort.test.js
@@ -1,4 +1,4 @@
-const bubbleSort = require("./bubbleSort");
+const bubbleSort = require("../bubbleSort");
 
 test("bubbleSort should sort [3,2,5,1,0,19] to [0,1,2,3,5,19]", () => {
   expect(bubbleSort([3, 2, 5, 1, 0, 19])).toEqual([0, 1, 2, 3, 5, 19]);
